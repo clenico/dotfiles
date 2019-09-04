e25441cf5a67ca3879d5dd7d2523eccd0dbf756a
clement nicolas <niccle27@gmail.com>
2019-09-04 10:36:06 +0200

    add locate fuzzy search ranger

--- Ranger/.config/ranger/commands.py
+++ Ranger/.config/ranger/commands.py
@@ -1,4 +1,5 @@
 from ranger.api.commands import Command
+import os
 
 class paste_as_root(Command):
 	def execute(self):
@@ -32,6 +33,31 @@ class fzf_select(Command):
         stdout, stderr = fzf.communicate()
         if fzf.returncode == 0:
             fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
+# fzf_locate
+class fzf_locate(Command):
+    """
+    :fzf_locate
+
+    Find a file using fzf.
+
+    With a prefix argument select only directories.
+
+    See: https://github.com/junegunn/fzf
+    """
+    def execute(self):
+        import subprocess
+        if self.quantifier:
+            command="locate home media | fzf -e -i"
+        else:
+            command="locate home media | fzf -e -i"
+        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
             if os.path.isdir(fzf_file):
                 self.fm.cd(fzf_file)
             else:
