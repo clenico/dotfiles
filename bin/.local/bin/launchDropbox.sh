b84ac3cbb85d6da25fe2b65e90269fb088fb0da5
clement nicolas <niccle27@gmail.com>
2019-09-05 23:47:03 +0200

    add launch Dropboxs script

---
+++ bin/.local/bin/launchDropbox.sh
@@ -0,0 +1,5 @@
+# HOME=~/.clouds/Dropbox/School dropbox start -i &
+# HOME=~/.clouds/Dropbox/Personnal dropbox start -i &
+# sleep 5
+HOME=~/.clouds/Dropbox/School /home/niccle27/.clouds/Dropbox/School/.dropbox-dist/dropboxd &
+HOME=~/.clouds/Dropbox/Personnal /home/niccle27/.clouds/Dropbox/Personnal/.dropbox-dist/dropboxd &
