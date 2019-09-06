79ae908d7235eba001f88295e71a87c06f1a3313
clement nicolas <niccle27@gmail.com>
2019-09-06 20:41:40 +0200

    dropbox autostart try to fix

--- bin/.local/bin/autostart.sh
+++ bin/.local/bin/autostart.sh
@@ -103,9 +103,8 @@ fi
 ~/.local/bin/RandomWallpaper.sh &
 
 if have dropbox; then
-    HOME=~/.clouds/Dropbox/School dropbox start -i &
-    # HOME=~/.clouds/Dropbox/Personnal dropbox start -i &
+    HOME=~/.clouds/Dropbox/Personnal dropbox start -i &
+    # HOME=~/.clouds/Dropbox/School dropbox start -i &
     sleep 5
-    HOME=~/.clouds/Dropbox/School /home/niccle27/.clouds/Dropbox/School/.dropbox-dist/dropboxd
-    HOME=~/.clouds/Dropbox/Personnal /home/niccle27/.clouds/Dropbox/Personnal/.dropbox-dist/dropboxd
+    ~/.local/bin/launchDropbox.sh
 fi
