ef83c8b8cebe02a6e230144a6322aae9c8910290
niccle27 <niccle27@gmail.com>
2020-08-17 00:10:15 +0200

    add script rofi-screenlayout

--- MyScripts/MyScripts/rofi-screenlayout.sh
+++ MyScripts/MyScripts/rofi-screenlayout.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 #
-# Allows you to find your local files.
-#
-# NOTE: this script use `locate` instead of `find` for better
-# performance. Thus, be careful to not forget to update your database
-# for `mlocate`.
+# Allows choose the screenlayout you want. You can easily create one
+# using arandr
 
 set -e
 
 path="/home/$USER/.screenlayout/"
-target=$(ls $path | rofi -dmenu -width 90 -i -p "Find:")
+target=$(ls $path | rofi -dmenu -width 90 -i -p "Screen layout")
 "$path$target"
