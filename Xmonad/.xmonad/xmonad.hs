17568f9344fbcef22d1e485cd33059b49d2f52d4
niccle27 <niccle27@gmail.com>
2020-08-24 22:34:10 +0200

    clean startup

--- Xmonad/.xmonad/xmonad.hs
+++ Xmonad/.xmonad/xmonad.hs
@@ -267,13 +267,14 @@ myStartupHook = do
     -- spawn "$HOME/.xmonad/scripts/autostart.sh"
     spawnOnce "exec trayer --align right --widthtype request --padding 0 --SetDockType true --SetPartialStrut true --expand true --transparent true --alpha 0 --tint 0x292d3e --height 26 --margin 5 --edge bottom --distance 0 --monitor  \"primary\""
     spawnOnce "/home/niccle27/MyScripts/autostart.sh > ~/.output/autostart.sh"
-    spawnOnOnce "NSP" "gnome-calendar"
+    spawnOnOnce "7" "korganizer"
     spawnOnOnce "NSP" "surf https://web.whatsapp.com/"
     spawnOnOnce "NSP" "messenger-nativefier"
     spawnOnOnce "NSP" "discord"
     spawnOnOnce "NSP" "skypeforlinux"
     spawnOnOnce "2" myFileManager
     spawnOnOnce "3" "firefox"
+    spawnOnOnce "8" "emacs"
     setWMName "LG3D"
 
 -- IDK wth is that
