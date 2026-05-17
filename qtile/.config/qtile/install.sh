873c15f1b5ab733ef99f13f0cf82365fbe4d6552
clenico <clement.nicolas.1996@gmail.com>
2026-05-17 20:55:44 +0200

    [IMP] qtile add install script

---
+++ qtile/.config/qtile/install.sh
@@ -0,0 +1,8 @@
+uv tool install --force qtile \
+  --with psutil \
+  --with pulsectl \
+  --with pulsectl-asyncio \
+  --with dbus-fast \
+  --with cairocffi \
+  --with xcffib \
+  --with pangocffi
